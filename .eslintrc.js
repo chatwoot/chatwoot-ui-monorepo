b8aa210064bcf43df5b65e5b04f7a391aae0dbe4
Pranav Raj S <pranav@chatwoot.com>
2023-11-06 12:15:09 -0800

    Update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,55 @@
 module.exports = {
-  extends: [
-    'airbnb-base/legacy',
-    'plugin:vue/recommended',
-    'prettier',
-  ],
+  extends: ['airbnb-base/legacy', 'plugin:vue/recommended', 'prettier'],
   plugins: ['prettier', '@stylistic/js'],
-  ignorePatterns: ['**/*/dist/**/*'],
+  ignorePatterns: ['**/*/dist/**/*', '**/*/node_modules/'],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2024,
   },
   rules: {
-    "comma-dangle": ["error", 'only-multiline'],
+    'prettier/prettier': ['error'],
     camelcase: 'off',
     'no-param-reassign': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-named-as-default': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/label-has-for': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'import/no-unresolved': 'off',
+    'vue/html-indent': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 20,
+        },
+        multiline: {
+          max: 1,
+        },
+      },
+    ],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+    'vue/no-v-html': 'off',
+    'vue/component-definition-name-casing': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'import/extensions': ['off'],
+    'no-console': 'error',
+    'comma-dangle': ['error', 'only-multiline'],
   },
   env: {
     browser: true,
