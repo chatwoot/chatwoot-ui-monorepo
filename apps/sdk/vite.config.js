4ab7ddb631b957e89a454fdb633562fb3ff9bf35
Pranav Raj S <pranav@chatwoot.com>
2023-11-06 08:23:25 -0800

    Bundle assets together

--- apps/sdk/vite.config.js
+++ apps/sdk/vite.config.js
@@ -1,7 +1,9 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
+  plugins: [cssInjectedByJsPlugin()],
   build: {
     lib: {
       entry: resolve(__dirname, 'main.js'),
