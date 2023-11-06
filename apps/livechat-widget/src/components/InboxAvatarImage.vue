555e66c80303df9bb045c203008386345f6a298c
Pranav Raj S <pranav@chatwoot.com>
2023-11-06 11:25:28 -0800

    Fallback for images

---
+++ apps/livechat-widget/src/components/InboxAvatarImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <img
+    v-if="!hasImageLoadError"
+    :src="avatarUrl"
+    alt="avatar"
+    @error="onImageLoadError"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    avatarUrl: {
+      type: String,
+      required:true
+    }
+  },
+  data() {
+    return { hasImageLoadError: false };
+  },
+  methods: {
+    onImageLoadError() {
+      this.hasImageLoadError = true
+    }
+  },
+}
+</script>
